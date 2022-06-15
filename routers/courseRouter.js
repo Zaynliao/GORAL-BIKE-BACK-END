33d5abdd94aae4c822e346f2ddc2e1b71a706a82
kiyo415411 <barbie841111@gmail.com>
2022-06-15 11:05:23 +0800

    [UPDATE] 課程列表串資料庫

--- routers/courseRouter.js
+++ routers/courseRouter.js
@@ -8,7 +8,7 @@ const pool = require('../utils/db');
 // 取得 course 列表
 router.get('/', async (req, res, next) => {
   let [data, fields] = await pool.execute(
-    'SELECT classes.course_id, classes.course_category_id, classes.course_title, classes.course_pictures, classes.course_location_id, classes.course_date, classes.course_status_id, classes.course_price, classes.course_content, classes.course_inventory, classes.course_valid FROM classes LEFT JOIN course_category on classes.course_category_id = course_category.course_category_id WHERE classes.course_valid = ?',
+    'SELECT * FROM classes LEFT JOIN course_category on classes.course_category_id = course_category.course_category_id WHERE classes.course_valid = ?',
     [1]
   );
   res.json(data);
