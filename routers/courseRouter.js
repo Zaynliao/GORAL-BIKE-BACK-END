22a28f4bc69f52c6fb29bc6d85fbd1285ef58a77
kiyo415411 <barbie841111@gmail.com>
2022-06-17 16:06:39 +0800

    [UPDATE courseRouter 增加報名類別篩選

--- routers/courseRouter.js
+++ routers/courseRouter.js
@@ -1,4 +1,4 @@
-const { application } = require('express');
+const { response, application } = require('express');
 const express = require('express');
 const router = express.Router();
 
@@ -6,27 +6,25 @@ const router = express.Router();
 const pool = require('../utils/db');
 
 router.get('/', async (req, res, next) => {
-  let [data, fields] = await pool.execute(
-    'SELECT * FROM classes WHERE course_valid = ?',
-    [1]
-  );
-
+  // 取得目前狀態
+  let statu = req.query.statu || 1;
   // 取得目前在第幾頁
   let page = req.query.page || 1;
-  // 總筆數
-  const total = data.length;
   // 一頁幾筆
-  const perPage = 7;
-  // 總頁數
-  const lastPage = Math.ceil(total / perPage);
+  const perPage = 9;
   // 計算每頁跳過幾筆顯示
   const offset = (page - 1) * perPage;
   // 取得當頁資料
 
   let [pageResults] = await pool.execute(
-    'SELECT * FROM classes, course_category, course_location, course_status, venue WHERE classes.course_valid = ? AND classes.course_category_id = course_category.course_category_id AND classes.course_location_id = course_location.course_location_id AND classes.course_status_id = course_status.course_status_id AND course_location.course_venue_id = venue.id ORDER BY classes.course_date DESC LIMIT ? OFFSET ? ',
-    [1, perPage, offset]
+    `SELECT * FROM classes, course_category, course_location, course_status, venue WHERE classes.course_valid = ? AND classes.course_category_id = course_category.course_category_id AND classes.course_location_id = course_location.course_location_id AND classes.course_status_id = course_status.course_status_id AND course_location.course_venue_id = venue.id AND classes.course_status_id = ? ORDER BY classes.course_date DESC LIMIT ? OFFSET ? `,
+    [1, statu, perPage, offset]
   );
+
+  // 總筆數
+  const total = pageResults.length;
+  // 總頁數
+  const lastPage = Math.ceil(total / perPage);
 
   res.json({
     // 儲存跟頁碼有關的資料
