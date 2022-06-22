6779199589ded61c187a74e7d5c63259c1e386c2
kiyo415411 <barbie841111@gmail.com>
2022-06-22 10:41:43 +0800

    課程排序 SQL 語法

--- routers/courseRouter.js
+++ routers/courseRouter.js
@@ -5,6 +5,23 @@ const router = express.Router();
 const pool = require('../utils/db'); // 引入 db
 
 router.get('/', async (req, res, next) => {
+  let sortMethod = req.query.sortMethod || 'hotSort'; // 取得排序方法
+  {
+    switch (sortMethod) {
+      case 'hotSort':
+        sortMethodString = `ORDER BY (classes.course_enrollment/classes.course_inventory) DESC`;
+        break;
+      case 'newSort':
+        sortMethodString = `ORDER BY classes.course_date DESC`;
+        break;
+      case 'cheapSort':
+        sortMethodString = `ORDER BY classes.course_price ASC `;
+        break;
+      case 'expensiveSort':
+        sortMethodString = `ORDER BY classes.course_price DESC `;
+        break;
+    }
+  }
   let statu = req.query.statu || 1; // 取得目前狀態
   let price = req.query.priceSubmit || [0, 10000]; // 取得價錢篩選
   let person = req.query.personSubmit || [0, 100]; // 取得人數篩選
@@ -37,7 +54,7 @@ router.get('/', async (req, res, next) => {
   // ------------------------------------ 取得當頁資料
 
   let [pageResults] = await pool.execute(
-    `SELECT * FROM classes, course_category, course_location, course_status, venue WHERE classes.course_valid = ? AND classes.course_category_id = course_category.course_category_id AND classes.course_location_id = course_location.course_location_id AND classes.course_status_id = course_status.course_status_id AND course_location.course_venue_id = venue.id AND classes.course_status_id = ? AND classes.course_price BETWEEN ? AND ? AND classes.course_inventory BETWEEN ? AND ? ${categorySQLArray} ORDER BY classes.course_date DESC LIMIT ? OFFSET ? `,
+    `SELECT * FROM classes, course_category, course_location, course_status, venue WHERE classes.course_valid = ? AND classes.course_category_id = course_category.course_category_id AND classes.course_location_id = course_location.course_location_id AND classes.course_status_id = course_status.course_status_id AND course_location.course_venue_id = venue.id AND classes.course_status_id = ? AND classes.course_price BETWEEN ? AND ? AND classes.course_inventory BETWEEN ? AND ? ${categorySQLArray} ${sortMethodString} LIMIT ? OFFSET ? `,
     [1, statu, price[0], price[1], person[0], person[1], perPage, offset]
   );
 
