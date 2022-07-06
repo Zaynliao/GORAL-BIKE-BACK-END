1405ae7da727a77b98e39f8ddec4de4617007826
Eason <a28165965@gmail.com>
2022-07-06 20:59:09 +0800

    [UPDATE] 訂單細節頁api已做 未完成

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -24,4 +24,59 @@ router.get('/:userId', async (req, res) => {
   }
 });
 
+router.get('/detail/:orderId', async (req, res) => {
+  const orderId = req.params.orderId;
+
+  let [orderDetail] = await pool.execute(
+    `
+      SELECT order_list.*,order_status.order_status,payment_method.payment_method_name, payment_status.payment_status,delivery.delivery_method FROM order_list, payment_method, payment_status, order_status, delivery 
+      WHERE order_list.order_status = order_status.id
+      AND order_list.payment_method_id = payment_method.id
+      AND order_list.payment_status_id = payment_status.id
+      AND order_list.delivery_id = delivery.delivery_id
+      AND order_list.order_id = ?`,
+    [orderId]
+  );
+
+  let [orderProduct] = await pool.execute(
+    `
+      SELECT product_order.*, product.product_price, product.product_name, product.product_images 
+      FROM product_order,product 
+      WHERE order_id = ? 
+      AND product_order.product_id = product.product_id;`,
+    [orderId]
+  );
+  const orderProductDetail = orderProduct.map((item) => {
+    return {
+      id: item.product_id,
+      price: item.product_price,
+      name: item.product_name,
+      image: item.product_images,
+      quantity: item.quantity,
+    };
+  });
+  let [orderCourse] = await pool.execute(
+    `
+      SELECT course_order.*, classes.course_price, classes.course_title, classes.course_pictures
+      FROM course_order,classes 
+      WHERE order_id = ? 
+      AND course_order.course_id = classes.course_id;`,
+    [orderId]
+  );
+  let [orderActivity] = await pool.execute(
+    `
+      SELECT activity_order.*, activity.activity_fee, activity.activity_name, activity.activity_pictures 
+      FROM activity_order,activity 
+      WHERE order_id = ? 
+      AND activity_order.activity_id = activity.activity_id;`,
+    [orderId]
+  );
+  res.json({
+    data: orderDetail,
+    productData: orderProductDetail,
+    courseData: [],
+    activity: [],
+  });
+});
+
 module.exports = router;
