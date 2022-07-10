3dc87a4f29ce66e3f15b81d31e478657566ae378
june <barbie841111@gmail.com>
2022-07-10 15:31:26 +0800

    [UPDATE] 簡易導覽系統

--- server.js
+++ server.js
@@ -84,30 +84,30 @@ app.use((err, req, res, next) => {
   res.status(500).send('Server Error: 請洽系統管理員');
 });
 
-// app.listen(3001, () => {
-//   console.log('server start at 3001');
-// });
+app.listen(3001, () => {
+  console.log('server start at 3001');
+});
 
 //將 express 放進 http 中開啟 Server 的 3000 port ，正確開啟後會在 console 中印出訊息
-const server = require('http')
-  .Server(app)
-  .listen(3001, () => {
-    console.log('open server!');
-  });
+// const server = require('http')
+//   .Server(app)
+//   .listen(3001, () => {
+//     console.log('open server!');
+//   });
 
 //將啟動的 Server 送給 socket.io 處理
 // const io = require('socket.io')(server);
 
-const socket = require('socket.io');
-const io = socket(server, { cors: { origin: '*', methods: ['GET', 'POST'] } });
+// const socket = require('socket.io');
+// const io = socket(server, { cors: { origin: '*', methods: ['GET', 'POST'] } });
 
-//監聽 Server 連線後的所有事件，並捕捉事件 socket 執行
-io.on('connection', (socket) => {
-  //經過連線後在 console 中印出訊息
-  console.log('success connect!');
-  //監聽透過 connection 傳進來的事件
-  socket.on('getMessage', (message) => {
-    //回傳 message 給發送訊息的 Client
-    socket.emit('getMessage', message);
-  });
-});
+// //監聽 Server 連線後的所有事件，並捕捉事件 socket 執行
+// io.on('connection', (socket) => {
+//   //經過連線後在 console 中印出訊息
+//   console.log('success connect!');
+//   //監聽透過 connection 傳進來的事件
+//   socket.on('getMessage', (message) => {
+//     //回傳 message 給發送訊息的 Client
+//     socket.emit('getMessage', message);
+//   });
+// });
